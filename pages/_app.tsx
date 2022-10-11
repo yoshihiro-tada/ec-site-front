3444e82ae8e746364734a1c60d7943b1e2aa772d
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2022-10-11 23:50:50 +0900

    MUIの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CssBaseline } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    jssStyles?.parentElement?.removeChild(jssStyles)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>MyApp</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
-
-export default MyApp
