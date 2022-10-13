fa36b453404480d32e469ceb999b4b5c5394fb14
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2022-10-13 21:54:12 +0900

    loginページの準備

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,33 +1,11 @@
 import { Button } from '@material-ui/core'
 import Link from 'next/link'
 
-export default function Home() {
+export default function CostomerLoginForm() {
   return (
     <>
     <h1>Login Page</h1>
-    <div style={{ margin: '0.5em' }}>
-      <Link href="/">
-        <a>
-          <Button variant="contained" color="primary">home</Button>{' '}
-        </a>
-      </Link>
-    </div>
-    <div style={{ margin: '0.5em' }}>
-      <Button variant="contained" color="primary">Items</Button>{' '}
-    </div>
-    <div style={{ margin: '0.5em' }}>
-      <Button variant="contained" color="primary">Categories</Button>{' '}
-    </div>
-    <div style={{ margin: '0.5em' }}>
-      <Button variant="contained" color="primary">Editer Login</Button>{' '}
-    </div>
-    <div style={{ margin: '0.5em' }}>
-      <Link href="/blog">
-        <a>
-          <Button variant="contained" color="primary">Blog</Button>{' '}
-        </a>
-      </Link>
-    </div>
+
     </>
   )
 }
